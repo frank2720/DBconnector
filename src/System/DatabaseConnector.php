505d69b0bd21baf2c5024f78d60a6e2c90eb9376
frank2720 <otienof534@gmail.com>
2024-06-27 13:36:09 +0600

    first commit

---
+++ src/System/DatabaseConnector.php
@@ -0,0 +1,32 @@
+<?php
+namespace Src\System;
+
+class DatabaseConnector
+{
+    private $dbConnection = null;
+
+    public function __construct()
+    {
+        $host = getenv('DB_HOST');
+        $port = getenv('DB_PORT');
+        $db   = getenv('DB_DATABASE');
+        $user = getenv('DB_USERNAME');
+        $pass = getenv('DB_PASSWORD');
+
+        try {
+            $this->dbConnection = new \PDO(
+                sprintf("mysql:host=%s;port=%s;dbname=%s;charset=UTF8", $host,$port,$db ),
+                $user,
+                $pass
+            );
+        } catch (\PDOException $e) {
+            exit($e->getMessage());
+        }
+    }
+    
+
+    public function db()
+    {
+        return $this->dbConnection;
+    }
+}
